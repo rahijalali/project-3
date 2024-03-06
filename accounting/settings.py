3b21bc8a160a408a8ceecca88469f5ecd791c994
Raheleh Jalali <147407998+rahijalali@users.noreply.github.com>
2024-03-06 07:41:04 +0000

    Update settings.py set DEBUG mode to false

--- accounting/settings.py
+++ accounting/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-p!&sy7jbxx5up3%u_wwc+65r%&za6st45@7^*!)sk%dyzqd2uf'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*','localhost', '127.0.0.1:8000/','.herokuapp.com']
 
